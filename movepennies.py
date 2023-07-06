665c069d5c799a2c440e4eb627bb4ac2f3c43c1c
Pui Ho Wilson Tsang <tsangphwilson@gmail.com>
2023-07-06 19:18:20 +0000

    Thu, Jul 6, 2023, 7:18 PM +00:00

--- movepennies.py
+++ movepennies.py
@@ -1,10 +1,19 @@
+"""
+import all required modules
+"""
 import random
 
 def main():
+    """
+    main program
+    """
     starting_board = [4,3,2,1]
     print(one_move())
 
 def one_move():
+    """
+    plays one move
+    """
     moves = []
     for _ in range(4):
         moves.append(random.randint(0,1))
