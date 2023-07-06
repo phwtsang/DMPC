bf5e948d75a3dc087c61b22a19f7d3c03b133126
Pui Ho Wilson Tsang <tsangphwilson@gmail.com>
2023-07-06 00:40:17 +0000

    Thu, Jul 6, 2023, 12:40 AM +00:00

---
+++ whopickslast.py
@@ -0,0 +1,50 @@
+"""
+import required modules
+"""
+import random
+
+
+def main():
+    """
+    Starts with n flags.
+    Two players takes turn to remove 1, 2,or 3 flags.
+    Player who picks the last flag wins.
+    """
+    n_flags = 21
+    for _ in range(10):
+        print(play_one_game(n_flags))
+
+
+def play_one_game(n_flags):
+    """
+    Player take turns to pick flage until a winner is determined
+    """
+    count = n_flags
+    flags_remaining = []
+    player1 = []
+    player2 = []
+    while True:
+        if count <= 3:
+            player1_pick = count
+        else:
+            player1_pick = random.randint(1,3)
+        player1.append(player1_pick)
+        count = count - player1_pick
+        if count == 0:
+            print('Player1 has won')
+            break
+        if count <= 3:
+            player2_pick = count
+        else:
+            player2_pick = random.randint(1,3)
+        player2.append(player2_pick)
+        count = count - player2_pick
+        if count == 0:
+            print('Player2 has won')
+            break
+        flags_remaining.append(count)
+        #print(player1_pick, player2_pick)
+    return flags_remaining
+
+
+main()
