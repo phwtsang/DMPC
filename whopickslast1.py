b91c147ce4fa990d20f49fb16ecf6188cc8ca483
Pui Ho Wilson Tsang <tsangphwilson@gmail.com>
2023-07-06 03:50:36 +0000

    Thu, Jul 6, 2023, 3:50 AM +00:00

---
+++ whopickslast1.py
@@ -0,0 +1,78 @@
+"""
+import required modules
+"""
+import random
+import csv
+
+
+def main():
+    """
+    Starts with n_flags flags.
+    Two players takes turn to remove 1, 2,or 3 flags.
+    Player who picks the last flag wins.
+    Create random_pick() to reduce duplication.
+    """
+    n_flags = 21
+    player1_wins = 0
+    player2_wins = 0
+    my_file = open('results.csv', 'w')
+    writer = csv.writer(my_file)
+    writer.writerow(['Player1', 'Player2', 'Flags Remaining', 'Winner'])
+    for _ in range(10000):
+        results = play_one_game(n_flags)
+        if results['Winner'] == 'Player1':
+            player1_wins +=1
+        else:
+            player2_wins +=1
+        #print(results)
+        writer.writerow([results['Player1'], results['Player2'], results['Flags Remaining'], results['Winner']])
+
+    print('Player1 wins:',player1_wins,'Player2 wins:',player2_wins)
+    my_file.close()
+
+
+def play_one_game(n_flags):
+    """
+    Player take turns to pick flage until a winner is determined
+    Both players pick randomly until there are 3 or fewer flags remain
+    """
+    count = n_flags
+    flags_remaining = []
+    player1 = []
+    player2 = []
+    results = {}
+    while True:
+        if count <= 3:
+            player1_pick = count
+        else:
+            player1_pick = random.randint(1,3)
+        player1.append(player1_pick)
+        count = count - player1_pick
+        if count == 0:
+            #print('Player1 has won')
+            #print(player1)
+            #print(player2)
+            results['Player1'] = player1
+            results['Player2'] = player2
+            results['Winner'] = 'Player1'
+            break
+        if count <= 3:
+            player2_pick = count
+        else:
+            player2_pick = random.randint(1,3)
+        player2.append(player2_pick)
+        count = count - player2_pick
+        if count == 0:
+            #print('Player2 has won')
+            #print(player1)
+            #print(player2)
+            results['Player1'] = player1
+            results['Player2'] = player2
+            results['Winner'] = 'Player2'
+            break
+        flags_remaining.append(count)
+        results['Flags Remaining'] = flags_remaining
+    return results
+
+
+main()
