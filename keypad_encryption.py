1ed4ec8aa4c31c1d13014e07d916fe0d3ec31d1e
Pui Ho Wilson Tsang <tsangphwilson@gmail.com>
2023-07-10 21:29:32 +0000

    Mon, Jul 10, 2023, 9:29 PM +00:00

--- keypad_encryption.py
+++ keypad_encryption.py
@@ -1,16 +1,16 @@
 """
 This program implements the phone keypad encrytion strategy
 """
-def main():
-    """
-    This is main()
-    """
-    keypad_dict = {
+KEYPAD_DICT = {
         2:['a', 'b', 'c'], 3:['d', 'e', 'f'], 4:['g', 'h', 'i'],
         5:['j', 'k', 'l'], 6:['m', 'n', 'o'], 7:['p', 'q', 'r', 's'],
         8:['t', 'u', 'v'], 9:['w', 'x', 'y', 'z']
     }
 
+def main():
+    """
+    This is main()
+    """
     to_do = input('Encryption (e) or Decryption (d): ')
     if to_do == 'e':
         output = encrypt(input('Enter plain text: '))
