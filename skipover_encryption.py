590d84c00b33a6b69ede8a1c478f162c7f012843
Pui Ho Wilson Tsang <tsangphwilson@gmail.com>
2023-07-12 02:18:12 +0000

    Wed, Jul 12, 2023, 2:18 AM +00:00

--- skipover_encryption.py
+++ skipover_encryption.py
@@ -23,7 +23,7 @@ def encrypt(input_text):
     input_list = list(input_text)
     print(input_list)
     for i,char in enumerate(input_text):
-        output_text[i] = char + output_text
+        output_text[(i+4)%4] = char
     return output_text
 
 def decrypt(input_text):
