ffed5d7296384e01669fb63ce60e5d5039af7e3f
Pui Ho Wilson Tsang <tsangphwilson@gmail.com>
2023-07-12 03:10:57 +0000

    Wed, Jul 12, 2023, 3:10 AM +00:00

--- skipover_encryption.py
+++ skipover_encryption.py
@@ -8,28 +8,30 @@ def main():
     choice = input('Encrytion (e) or Decryption (d): ')
     if choice == 'e':
         input_text = input('Enter plaintext: ')
-        output = encrypt(input_text)
+        skip_key = input('Enter Skip Key: ')
+        output = encrypt(input_text, skip_key)
         print(output)
     else:
         input_text = input('Enter cyphertext: ')
-        output = decrypt(input_text)
+        skip_key = input('Enter Skip Key: ')
+        output = decrypt(input_text, skip_key)
         print(output)
 
-def encrypt(input_text):
+def encrypt(input_text, skip_key):
     """
     implement the skip-over encryption algorithm
     """
     output_text = ''
     output_list = list(input_text)
     input_list = list(input_text)
-    print(input_list)
+    #print(input_list)
     for i,char in enumerate(input_list):
-        list_index = (i*4)%11
+        list_index = (i*skip_key)%11
         print(i, list_index)
         output_list[list_index] = char
     return output_text.join(output_list)
 
-def decrypt(input_text):
+def decrypt(input_text, skip_key):
     """
     implement the skip-over encryption algorithm
     """
