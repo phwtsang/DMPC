d06de6295abb4c318480758549810d757df3cd86
Pui Ho Wilson Tsang <tsangphwilson@gmail.com>
2023-07-13 05:22:05 +0000

    Thu, Jul 13, 2023, 5:22 AM +00:00

--- permutation_encryption.py
+++ permutation_encryption.py
@@ -35,7 +35,7 @@ def perm_decrypt():
     for i, num in enumerate(input_list):
         i_mod = i%key_len
         output_list.append(key_list[i_mod])
-    return ''.join(putput_list)
+    return ''.join(output_list)
 
 
 main()
