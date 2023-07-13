50a300772cc475e5a53d9e031a3ebd1e18a8001f
Pui Ho Wilson Tsang <tsangphwilson@gmail.com>
2023-07-13 03:57:20 +0000

    Thu, Jul 13, 2023, 3:57 AM +00:00

---
+++ permutation_encryption.py
@@ -0,0 +1,20 @@
+"""
+permutation encryption
+"""
+def main():
+    """
+    main function
+    """
+    pass
+
+def perm_encrypt():
+    """
+    perm_encrypt function
+    """
+    pass
+
+def perm_decrypt():
+    """
+    perm_decrypt function
+    """
+    pass
